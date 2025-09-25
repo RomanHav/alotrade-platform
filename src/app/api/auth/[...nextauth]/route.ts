36e475eeaac84bd76d26959780e9e7913780db3b
Roman_Havrylov <146737481+RomanHav@users.noreply.github.com>
2025-09-25 13:03:08 +0300

    add some changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
